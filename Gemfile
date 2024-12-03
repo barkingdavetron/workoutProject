17db776014bfb860dc7a9d1184b54b572b9edc09
david <mcgluedavid@gmail.com>
2024-12-03 21:12:33 +0000

    added devise for user information

--- Gemfile
+++ Gemfile
@@ -33,10 +33,10 @@ gem "bootsnap", require: false
 
 # Deploy this application anywhere as a Docker container [https://kamal-deploy.org]
 gem "kamal", require: false
-
+gem "devise"
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
-gem 'rack-cors'
+gem "rack-cors"
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,31 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_02_231858) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_03_203355) do
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "password"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  create_table "userworkouts", force: :cascade do |t|
-    t.string "exercise"
-    t.integer "User_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["User_id"], name: "index_userworkouts_on_User_id"
-  end
-
-  create_table "workouts", force: :cascade do |t|
-    t.string "activity"
-    t.integer "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_workouts_on_user_id"
-  end
-
-  add_foreign_key "userworkouts", "Users"
-  add_foreign_key "workouts", "users"
 end
