8f7562aa24d1e14b952590bc771448fbacaa093a
david <mcgluedavid@gmail.com>
2024-12-04 00:09:07 +0000

    Add sign-out functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,10 @@ class UsersController < ApplicationController
   # GET /users/1/edit
   def edit
   end
+  def sign_out
+    reset_session
+    redirect_to root_path, notice: 'Signed out successfully.'
+  end
 
   # POST /users or /users.json
   def create
